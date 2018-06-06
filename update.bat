832b4828dcf15a8951dc0d6aa2fab4e1641cb67b
Adam.Yao <yaodwwy@gmail.com>
2018-06-06 14:57:03 +0800

    init blog

--- update.bat
+++ update.bat
@@ -20,12 +20,12 @@ pause
 ECHO %date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%  >>  AutogitPull.log
 call echo %%i %%date%% %%time%%	>>	AutogitPull.log
 ::执行更新
-cd C:\dev\IdeaProjects\gomro.mid
+cd C:\dev\IdeaProjects\mid
 
 ECHO C:\dev\IdeaProjects\gomro.mid >>  AutogitPull.log
 git pull  >>  AutogitPull.log
 
-cd C:\dev\IdeaProjects\gomro.web
+cd C:\dev\IdeaProjects\web
 
 ECHO C:\dev\IdeaProjects\gomro.web >>  AutogitPull.log
 git pull  >>  AutogitPull.log
